d25730bd65f220864d43703f27b38a6395a51bbc
DiAndjelo <satioidou2se@gmail.com>
2021-06-08 19:53:37 +0500

    add count of contributors to team list

--- backend/api/team/models.py
+++ backend/api/team/models.py
@@ -3,4 +3,5 @@ from django.db import models
 
 class Team(models.Model):
     name = models.CharField(verbose_name='Название команды', max_length=256)
-    creator = models.ForeignKey('account.Profile', verbose_name='Создатель команды', on_delete=models.SET_NULL, null=True, blank=True)
+    creator = models.ForeignKey('account.Profile', verbose_name='Создатель команды', on_delete=models.SET_NULL,
+                                null=True, blank=True)
--- backend/api/team/serializers.py
+++ backend/api/team/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 
+from backend.api.account.models import Profile
 from backend.api.team.models import Team
 
 
 class TeamSerializer(serializers.ModelSerializer):
+    count_of_contributors = serializers.SerializerMethodField('get_count_of_contributors')
+
     class Meta:
         model = Team
         fields = '__all__'
+
+    def get_count_of_contributors(self, obj):
+        count = Profile.objects.filter(teams__in=[obj.id, ]).count()
+        return count
