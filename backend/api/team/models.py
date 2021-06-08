dd9ddbce92f71104f92901fa0c8a1099c2bc3689
DiAndjelo <satioidou2se@gmail.com>
2021-06-08 19:14:12 +0500

    add creator to team model

--- backend/api/team/models.py
+++ backend/api/team/models.py
@@ -3,3 +3,4 @@ from django.db import models
 
 class Team(models.Model):
     name = models.CharField(verbose_name='Название команды', max_length=256)
+    creator = models.ForeignKey('account.Profile', verbose_name='Создатель команды', on_delete=models.SET_NULL, null=True, blank=True)
