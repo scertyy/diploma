cb321378c5d171fb54c588a0ded47fb2d9bc9eef
DiAndjelo <35653565Nick>
2021-06-22 04:39:35 +0500

    add task url

--- backend/api/team/views.py
+++ backend/api/team/views.py
@@ -1,5 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
@@ -13,6 +13,11 @@ class TeamViewSet(viewsets.ModelViewSet):
     serializer_class = TeamSerializer
     queryset = Team.objects.filter(parent=None)
     permission_classes = (permissions.IsAuthenticated, )
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response({'success': 'deleted'}, status=status.HTTP_204_NO_CONTENT)
 
     # ToDo: ПЕРЕПИСАТЬ
     @action(detail=False, methods=['post'], permission_classes=(permissions.IsAuthenticated, ))
